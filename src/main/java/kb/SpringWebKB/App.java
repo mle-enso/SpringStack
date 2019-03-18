f79ea7ec924b7858436c7ebe31cc6b676c1a9dd2
NashPL <nash25pl@gmail.com>
2019-03-18 23:10:47 +0000

    Fisrt Commit

---
+++ src/main/java/kb/SpringWebKB/App.java
@@ -0,0 +1,14 @@
+package kb.SpringWebKB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
